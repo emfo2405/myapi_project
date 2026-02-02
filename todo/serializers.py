35735eda238184088ea522560bcce371f4c23341
emfo2405 <emfo2405@student.miun.se>
2026-02-02 21:02:40 +0100

    status uppdelad i två delar

--- todo/serializers.py
+++ todo/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from .models import Todo
 
 class TodoSerializer(serializers.ModelSerializer):
+
+    status_display = serializers.CharField(
+        source="get_status_display",
+        read_only=True
+    )
+
     class Meta:
         model = Todo
-        fields = '__all__'
+        fields = 'id', 'title', 'description', 'status', 'status_display'
